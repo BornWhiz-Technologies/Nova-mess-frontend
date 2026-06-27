88595eb9408ebbe620c55988cc7706ee942c0670
gunapriya325 <gunapriya325@gmail.com>
2026-06-27 11:36:08 +0530

    conflict fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Login } from './login/login';
 import { Signup } from './signup/signup';
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'about', component: About },
     { path: 'contact', component: Contact },
+    { path: 'login', component: Login },
     { path: 'signup', component: Signup }
 ];
 
