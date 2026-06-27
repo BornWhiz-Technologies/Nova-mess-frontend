eb974101bf21dd0d7e621e4d45d17cbf6a4bd837
hema-kumar092 <kumarhema092@gmail.com>
2026-06-27 11:09:22 +0530

    navbar and loginpage changes then components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Login } from './login/login';
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'about', component: About },
-    { path: 'contact', component: Contact }
+    { path: 'contact', component: Contact },
+    { path: 'login', component: Login }
 ];
 
 
