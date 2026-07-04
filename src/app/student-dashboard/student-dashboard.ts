b60869d290135b00dc92d60fef68b709e4db2b00
gunapriya325 <gunapriya325@gmail.com>
2026-07-04 14:37:51 +0530

    studentdashboard ui  created

--- src/app/student-dashboard/student-dashboard.ts
+++ src/app/student-dashboard/student-dashboard.ts
@@ -1,9 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-student-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student-dashboard.html',
   styleUrl: './student-dashboard.css',
 })
-export class StudentDashboard {}
+export class StudentDashboard {
+  studentName = '';
+  rollNumber = '';
+  sidebarItems = [
+    'Dashboard',
+    "Today's Menu",
+    'Meal Booking',
+    'Attendance',
+    'Payment History',
+    'Feedback',
+    'Profile',
+    'Logout',
+  ];
+  quickActions = ['Book Meal', 'View Attendance', 'Pay Dues', 'Submit Feedback'];
+}
